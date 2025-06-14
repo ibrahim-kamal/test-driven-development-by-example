dd0412967feadbdcbb578cd29925967886848e7b
ibrahim kamal <ibrahim.kamal2997@gmail.com>
2025-06-14 22:15:05 +0300

    pdf

--- TDD.Money.Test/obj/Debug/net8.0/TDD.Money.Test.AssemblyInfo.cs
+++ TDD.Money.Test/obj/Debug/net8.0/TDD.Money.Test.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TDD.Money.Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd0d7b73dc505e03b567564b2f6d03a04537ca61")]
 [assembly: System.Reflection.AssemblyProductAttribute("TDD.Money.Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TDD.Money.Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
